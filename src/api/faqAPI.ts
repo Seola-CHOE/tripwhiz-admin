178e323fa1cd840197a81ab7f7358d6e0003a61b
USER <ckalswn371004@gmail.com>
2024-10-24 14:32:37 +0900

    modi,add 수정

--- src/api/faqAPI.ts
+++ src/api/faqAPI.ts
@@ -1,7 +1,7 @@
 // api/faqAPI.ts
 
 import axios from 'axios';
-import { IFaq } from '../types/faq';
+import { ICategory, IFaq } from '../types/faq';
 
 
 // API 호스트 URL 설정
@@ -20,8 +20,6 @@ export const getFaqList = async (): Promise<IFaq[]> => {
 
     console.log(`Request URL: ${host}/list`);
 
-    const res = await axios.get<{ dtoList: IFaq[] }>(`${host}/list`, header);
-
     console.log('Response Data:', res.data);
 
     return res.data.dtoList;
@@ -31,13 +29,3 @@ export const getFaqList = async (): Promise<IFaq[]> => {
   }
 };
 
-// 카테고리 리스트를 가져오는 함수
-export const getCategoryList = async (): Promise<ICategory[]> => {
-  try {
-    const res = await axios.get<{ dtoList: ICategory[] }>(`${host}/categories`);
-    return res.data.dtoList;
-  } catch (error) {
-    console.error('Failed to fetch category list:', error);
-    throw error;
-  }
-};
